dda4ff2dc69b95ed0d75e6249aa82a813bcb2f2f
andresds <andre_smiths@outlook.com>
2020-05-29 19:48:07 -0300

    componetizando o elemento date

--- src/app/shared/components/campos/input-date/input-date.component.ts
+++ src/app/shared/components/campos/input-date/input-date.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ValidacaoService } from '../validacao.service';
+import { FormGroup, AbstractControl } from '@angular/forms';
 
 @Component({
-  selector: 'input-date',
+  selector: 'app-input-date',
   templateUrl: './input-date.component.html',
   styleUrls: ['./input-date.component.css']
 })
 export class InputDateComponent implements OnInit {
 
-  constructor() { }
+  @Input() formGroup: FormGroup;
+  @Input() controlName: string;
+  @Input() titulo: string;
+
+  constructor( public validacao: ValidacaoService) { }
 
   ngOnInit(): void {
+  }
+
+  get formControl(): AbstractControl {
+    return this.formGroup.controls[this.controlName];
   }
 
 }
--- src/app/shared/components/campos/input-number/input-number.component.ts
+++ src/app/shared/components/campos/input-number/input-number.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, AbstractControl } from '@angular/forms';
+import { ValidacaoService } from '../validacao.service';
 
 @Component({
-  selector: 'input-number',
+  selector: 'app-input-number',
   templateUrl: './input-number.component.html',
   styleUrls: ['./input-number.component.css']
 })
 export class InputNumberComponent implements OnInit {
 
-  constructor() { }
+  @Input() formGroup: FormGroup;
+  @Input() controlName: string;
+  @Input() titulo: string;
+  @Input() minimo = 0;
+  @Input() maximo = 10;
+  @Input() step = 1;
+
+  constructor( public validacao: ValidacaoService) { }
 
   ngOnInit(): void {
+  }
+
+  get formControl(): AbstractControl {
+    return this.formGroup.controls[this.controlName];
   }
 
 }
