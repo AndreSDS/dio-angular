53537d6d791a66d794ec6a6c9d99126bd0709fed
andresds <andre_smiths@outlook.com>
2020-05-29 00:57:06 -0300

    editando html e arquivo.ts do componente

--- src/app/shared/components/campos/input-text-area/input-text-area.component.ts
+++ src/app/shared/components/campos/input-text-area/input-text-area.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, AbstractControl } from '@angular/forms';
+import { ValidacaoService } from '../validacao.service';
 
 @Component({
-  selector: 'input-text-area',
+  selector: 'app-input-text-area',
   templateUrl: './input-text-area.component.html',
   styleUrls: ['./input-text-area.component.css']
 })
 export class InputTextAreaComponent implements OnInit {
 
-  constructor() { }
+  @Input() formGroup: FormGroup;
+  @Input() controlName: string;
+  @Input() titulo: string;
+
+  constructor( private validacao: ValidacaoService) { }
 
   ngOnInit(): void {
+  }
+
+  get formControl(): AbstractControl {
+    return this.formGroup.controls[this.controlName];
   }
 
 }
