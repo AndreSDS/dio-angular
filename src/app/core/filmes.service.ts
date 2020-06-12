a07e1da29f012dfe63767ac5b3b6fd88cc5514d0
AndreSDS <andre_smiths@outlook.com>
2020-06-12 15:20:14 -0300

    iniciando o componente de visualizar filme

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -1,30 +1,42 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { take } from 'rxjs/operators';
-import { Filme } from './../shared/models/filme';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { take } from "rxjs/operators";
+import { Filme } from "./../shared/models/filme";
 
-const url = 'http://localhost:3000/filmes/';
+const url = "http://localhost:3000/filmes/";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class FilmesService {
-
-  constructor( private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   salvar(filme: Filme): Observable<Filme> {
     return this.http.post<Filme>(url, filme).pipe(take(1));
   }
 
-  listar(pagina: number, itensNaPagina: number, nomedoFilme: string, genero: string): Observable<Filme[]> {
+  listar(
+    pagina: number,
+    itensNaPagina: number,
+    nomedoFilme: string,
+    genero: string
+  ): Observable<Filme[]> {
     let httpParams = new HttpParams();
-    httpParams = httpParams.set('_page', pagina.toString());
-    httpParams = httpParams.set('_limit', itensNaPagina.toString());
-    httpParams = httpParams.set('_sort', 'id');
-    httpParams = httpParams.set('_order', 'desc');
-    if (nomedoFilme) { httpParams = httpParams.set('q', nomedoFilme); }
-    if (genero) { httpParams = httpParams.set('genero', genero); }
+    httpParams = httpParams.set("_page", pagina.toString());
+    httpParams = httpParams.set("_limit", itensNaPagina.toString());
+    httpParams = httpParams.set("_sort", "id");
+    httpParams = httpParams.set("_order", "desc");
+    if (nomedoFilme) {
+      httpParams = httpParams.set("q", nomedoFilme);
+    }
+    if (genero) {
+      httpParams = httpParams.set("genero", genero);
+    }
     return this.http.get<Filme[]>(url, { params: httpParams }).pipe(take(1));
+  }
+
+  visualizar(id: number): Observable<Filme> {
+    return this.http.get<Filme>(url + id);
   }
 }
--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,17 +1,16 @@
-import { Filme } from './../../shared/models/filme';
-import { FilmesService } from './../../core/filmes.service';
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
-import { pipe } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { Filme } from "./../../shared/models/filme";
+import { FilmesService } from "./../../core/filmes.service";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { take } from "rxjs/operators";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'dio-listagem-filmes',
-  templateUrl: './listagem-filmes.component.html',
-  styleUrls: ['./listagem-filmes.component.scss']
+  selector: "dio-listagem-filmes",
+  templateUrl: "./listagem-filmes.component.html",
+  styleUrls: ["./listagem-filmes.component.scss"]
 })
 export class ListagemFilmesComponent implements OnInit {
-
   filmes: Filme[] = [];
   pagina = 0;
   nomedoFilme: string;
@@ -22,44 +21,65 @@ export class ListagemFilmesComponent implements OnInit {
 
   constructor(
     private filemService: FilmesService,
-    private fb: FormBuilder
-  ) { }
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.filtrosListagem = this.fb.group({
-      texto: [''],
-      genero: ['']
+      texto: [""],
+      genero: [""]
     });
 
-    this.filtrosListagem.get('texto').valueChanges.pipe(take(1)).subscribe((val: string) => {
-      this.nomedoFilme = val;
-      this.resetarConsulta();
-    });
+    this.filtrosListagem
+      .get("texto")
+      .valueChanges.pipe(take(1))
+      .subscribe((val: string) => {
+        this.nomedoFilme = val;
+        this.resetarConsulta();
+      });
 
-    this.filtrosListagem.get('genero').valueChanges.pipe(take(1)).subscribe((val: string) => {
-      this.genero = val;
-      this.resetarConsulta();
-    });
+    this.filtrosListagem
+      .get("genero")
+      .valueChanges.pipe(take(1))
+      .subscribe((val: string) => {
+        this.genero = val;
+        this.resetarConsulta();
+      });
 
-    this.generos = ['Ação', 'Romance', 'Terror', 'Aventura', 'Ficção científica', 'Comédia', 'Drama'];
+    this.generos = [
+      "Ação",
+      "Romance",
+      "Terror",
+      "Aventura",
+      "Ficção científica",
+      "Comédia",
+      "Drama"
+    ];
 
     this.listarFilmes();
   }
 
+  onScroll(): void {
+    this.listarFilmes();
+  }
+
+  abrir(id: number): void {
+    this.router.navigateByUrl(`/filmes/${id}`);
+  }
+
   private listarFilmes() {
     this.pagina++;
-    this.filemService.listar(this.pagina, this.itensPorPagina, this.nomedoFilme, this.genero).subscribe((filmes: Filme[]) => {
-      this.filmes.push(...filmes);
-    });
+    this.filemService
+      .listar(this.pagina, this.itensPorPagina, this.nomedoFilme, this.genero)
+      .subscribe((filmes: Filme[]) => {
+        this.filmes.push(...filmes);
+      });
   }
 
   private resetarConsulta(): void {
     this.pagina = 0;
     this.filmes = [];
-    this.listarFilmes();
-  }
-
-  onScroll(): void {
     this.listarFilmes();
   }
 }
